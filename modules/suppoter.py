d76d26e7ce17c492699c15b869dcbdfdc9c5cf21
Orion <lcz2913196277@gmail.com>
2023-05-22 20:29:01 +0800

    增加了提取文件自动整理的功能

--- modules/suppoter.py
+++ modules/suppoter.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 def processor(path, outputBase, baseCommand, autherName, title, file):
     if autherName == "None":
@@ -41,6 +42,20 @@ def processor(path, outputBase, baseCommand, autherName, title, file):
         print("执行如下命令:")
         print(command)
         os.system(command)
+        
+        # delete .json, .tex and .tex-json files
+        print("删除多余文件...")
+        os.remove(outputPath + "/scene.json")
+        subfolders = os.listdir(outputPath)
+        # remove extra folders
+        for subfolder in subfolders:
+            if subfolder != "materials":
+                shutil.rmtree(outputPath + "/" + subfolder)
+        subfiles = os.listdir(outputPath + "/materials")
+        for subfile in subfiles:
+            if subfile.endwith(".png") or subfile.endwith(".gif") or subfile.endwith(".jpg") or subfile.endwith(".jpeg"):
+                shutil.move(outputPath + "/materials/" + subfile, outputPath)
+        shutil.rmtree(outputPath + "/materials")
         
     # else just copy files
     else:
